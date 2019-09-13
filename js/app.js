7bf47100c60d6500badbf829bd5d07f33fb5b6e1
Katie <47111708+katieatgeorgian@users.noreply.github.com>
2019-09-13 15:49:31 -0400

    added text and image tweet functionality

--- js/app.js
+++ js/app.js
@@ -25,3 +25,80 @@ document
   .addEventListener("change", handleFileSelect);
 
 // STEP 5 - Submit hosted url in Blackboard
+
+let tweets = [];
+const btn = document.getElementById("tweet");
+
+function tweeting(e) {
+  e.preventDefault();
+  let tweet = {};
+  let tweetText = document.getElementById("textarea").value;
+  let tweetImg = document.querySelector(".thumb").src;
+  tweet.tweet = tweetText;
+  tweet.image = tweetImg;
+
+  tweets.unshift(tweet);
+  display(tweets);
+  document.getElementById("textarea").value = "";
+  document.querySelector(".thumb").src = "";
+}
+
+function display(tweets) {
+  let newTweet = tweets
+    .map(function(tweet) {
+      //callback function
+      return `
+        <div class="form-group">
+              <hr />
+              <div class="column">
+                <div class="fb-start">
+                    <img
+                    id="avatar"
+                    src="https://instagram.fyyz1-1.fna.fbcdn.net/vp/e75c368a6e9624ff872ba315fac50e72/5DF17A92/t51.2885-19/11410505_833412053410486_294445979_a.jpg?_nc_ht=instagram.fyyz1-1.fna.fbcdn.net"
+                    alt=""
+                    />
+                    <p class="h6 pt-1">Katie H.</p>
+                    <a  href="#" class="username pl-2">@katieatgeorgian</a>
+                </div>
+                <div>
+                    <p class="tweetText">${tweet.tweet} </p>
+                    <img src="${tweet.image}" class="tweetImage">
+                </div>
+                <div class="fb tweetText">
+                    <section>
+                        <div id="inserts" class="btn-group mr-2">
+                            <button
+                            type="button"
+                            class="btn btn-secondary mdi mdi-comment-outline"
+                            aria-label="Reply"
+                            ></button>
+                            <button
+                            type="button"
+                            class="btn btn-secondary mdi mdi-twitter-retweet"
+                            aria-label="Retweet"
+                            ></button>
+                            <button
+                            type="button"
+                            class="btn btn-secondary mdi mdi-heart-outline"
+                            aria-label="Like"
+                            style=""
+                            ></button>
+                            <button
+                            type="button"
+                            class="btn btn-secondary mdi mdi-upload"
+                            aria-label="Share tweet"
+                            ></button>
+                        </div>
+                    </section>
+                </div>
+              </div>
+              
+              
+            </div>`;
+    })
+    .join("");
+
+  const main = document.querySelector("main");
+  main.innerHTML = newTweet;
+}
+btn.addEventListener("click", tweeting);
